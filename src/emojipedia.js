f7c9aa7bb17d2bc49e07433362b50214f621b160
HarshalDevmurari <92778686+HarshalDevmurari@users.noreply.github.com>
2022-07-23 17:47:42 +0530

    EmotiPedia Complete

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,10 +1,10 @@
 const emojipedia = [
   {
     id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
   },
   {
     id: 2,
@@ -19,6 +19,48 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "👻",
+    name: "Ghost",
+    meaning:
+      "Boo! A white, cartoon ghost making a silly face. Its tongue is stuck out and arms are outstretched, as if trying to scare someone in a friendly way. Several platforms feature one larger, bulging eye. "
+  },
+  {
+    id: 5,
+    emoji: "😏",
+    name: "Smirking Face",
+    meaning:
+      "As its official name reveals, Smirking Face represents the facial expression of a smirk. It’s used to communicate a range of feelings, including smugness, self-confidence, self-indulgence, mischief, cheeky humor, and general satisfaction."
+  },
+  {
+    id: 6,
+    emoji: "🖖",
+    name: "Vulcan Salute",
+    meaning:
+      "The image of a raised hand with the palm forward and the thumb extended, while the fingers are parted between the middle and ring finger is is the emoji symbol of the Vulcan salute popularized by the 1960s television series Star Trek. The hand gesture means “Live long and prosper!”."
+  },
+  {
+    id: 7,
+    emoji: "😷",
+    name: "Face with Medical Mask",
+    meaning:
+      "A yellow face wearing a white surgical mask, as used by health workers in hospitals as well as the general public during a pandemic. May be worn to avoid sickness or spreading airborne diseases."
+  },
+  {
+    id: 8,
+    emoji: "✊",
+    name: "Raised Fist",
+    meaning:
+      "A fist raised in the air that may be used as a celebratory gesture: the fist pump. Can also be used to represent zero items, due to a lack of any fingers being held up."
+  },
+  {
+    id: 9,
+    emoji: "👀",
+    name: "Eyes",
+    meaning:
+      "A pair of eyes, glancing slightly to the left on most platforms. The Google version of this emoji previously showed only a single eye, despite the name of this character being eyes (plural)."
   }
 ];
 
